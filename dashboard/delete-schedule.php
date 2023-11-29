6640dbd9c9773d513c4d9b007461061d4d720c87
KingJB13 <justin.bais13@gmail.com>
2023-11-29 12:37:27 +0800

    modified script source

--- dashboard/delete-schedule.php
+++ dashboard/delete-schedule.php
@@ -183,7 +183,7 @@ require_once '../configuration/dbcon.php';
         </div>
         
     </div>
-    <script src="/scripts/dashboard.js"></script>
+    <script src="../scripts/dashboard.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
