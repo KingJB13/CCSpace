58b3d1d3ca03f1e8b973962a6147918f2b080d46
KingJB13 <justin.bais13@gmail.com>
2023-12-10 14:56:15 +0800

    Created separate file for time-in and time-out qr-code scanners

---
+++ scripts/time-out.js
@@ -0,0 +1,50 @@
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
+Instascan.Camera.getCameras().then(function(cameras){
+    if(cameras.length > 0 ){
+        scanner.start(cameras[0]);
+    } else{
+        alert('No cameras found');
+    }
+}).catch(function(e) {
+    console.error(e);
+});
+
+scanner.addListener('scan',function(c){
+    document.getElementById('text').value = c;
+    document.getElementById('form').submit();
+    return false;
+});
+
+function fetchRequest(file, formData) {
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => {
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return res.json();
+    })
+    .then(result => {
+        if (result && result.length > 0 && result[0].symbol && result[0].symbol.length > 0 && result[0].symbol[0].data) {
+            const scannedData = result[0].symbol[0].data;
+            document.getElementById('text').value = scannedData;
+            document.getElementById('form').submit();
+
+        } else {
+            throw new Error("Invalid response format");
+        }
+    })
+    .catch(error => {
+        alert("Couldn't scan QR Code: " + error.message);
+    });
+}
+
+const file = document.getElementById('file')
+const form = document.querySelector('form');
+file.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
